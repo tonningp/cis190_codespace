99d8cdcb6ed35cce25d26ea7c49e7a1ab0f601a1
Paul Tonning <paul.tonning@student.vvc.edu>
2025-06-17 11:38:51 -0700

    add to summary

--- missions/week_1/module_1/demos/07.00.week_1.module_1.commands_0.sh
+++ missions/week_1/module_1/demos/07.00.week_1.module_1.commands_0.sh
@@ -1,3 +1,4 @@
+echo
 echo '* Use ps to list running processes in the current shell.'
 echo '* Use ps aux to see all processes with detailed information.'
 exec bash --rcfile $RCFILE
--- missions/week_1/module_1/demos/07.01.week_1.module_1.commands_1.sh
+++ missions/week_1/module_1/demos/07.01.week_1.module_1.commands_1.sh
@@ -1,3 +1,4 @@
+echo
 echo '* Type # hello world to create a comment.'
 echo '* Press enter, and you will see that the comment is not executed.'
 echo '* Try it again without the # symbol to see the difference.'
