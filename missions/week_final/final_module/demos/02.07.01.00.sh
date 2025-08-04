14035e032886538e8823a5ee75c4847cc2e790b4
Paul Tonning <paul.tonning@student.vvc.edu>
2025-08-03 21:11:03 -0700

    final project added

---
+++ missions/week_final/final_module/demos/02.07.01.00.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+# -*- coding: utf-8 -*-
+#export TRAINING_SHELL="1"
+#source "$TOP_DIR/colors.sh"
+#source "$TOP_DIR/emojis.sh"
+#source "$TOP_DIR/utils.sh"
+#source "$TOP_DIR/copy_data.sh"
+
+MODULE_NAME="Bash Essentials"
+source "$TOP_DIR/lesson_viewer.sh"
---
+++ missions/week_final/final_module/demos/06.07.01.00.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+
+MODULE_NAME="Redirection and Pipes"
+source "$TOP_DIR/lesson_viewer.sh"
